24a198607e813e5e11dfc4c3574bf6c93d139d72
Daniel Teixeira Pereira <97935464+danields91@users.noreply.github.com>
2022-03-14 14:12:15 -0300

    Início JS Básico

---
+++ script.js
@@ -0,0 +1,64 @@
+//alert("Hello World!")
+//console.log("Hello World!")
+
+/*var a;
+
+a = "HD";
+
+console.log(a);
+
+// Operador Ternário: Simplifica o "IF" (em algumas ocasiões)
+
+var idade = 16;
+
+var pode = idade >= 18 ? true : false; //Pode usar alguma palavra em string também, sempre na ordem "true : false"
+var pode = idade >= 18 ? "Pode" : "Não pode";
+
+console.log(pode)*/
+
+/*var nota1 = 8.0;
+var nota2 = 6.0;
+
+var media = (nota1 + nota2) / 2;
+
+var conceito = ""
+
+if (media >= 8) {
+    conceito = "Ótimo";
+}
+else if (media >= 6.5) {
+    conceito = "Bom";
+}
+else {
+    conceito = "Regular";
+}
+
+console.log(media);
+console.log(conceito);
+
+switch (conceito) {
+    case "Ótimo":
+        console.log('Parabéns, você é um ótimo aluno!')
+        break;
+    case "Bom":
+        console.log('Você está quase perfeito.')
+        break;
+    case "Regular":
+        console.log('Estude mais um pouco.')
+        break;
+    default:
+        console.log("Houve algum erro.")
+        break;
+}*/
+
+/*var alunos = ['Daniel','Camila', 'Arthur', "Alice", 'Helena']
+
+for (var i = 0; i < alunos.length; i++){
+    console.log(alunos[i])
+}*/
+
+var alunos = ['Daniel','Camila', 'Arthur', "Alice", 'Helena']
+
+for (var i in alunos){
+    console.log(alunos[i])
+}
